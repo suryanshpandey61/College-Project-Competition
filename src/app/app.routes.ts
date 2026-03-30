a14a41288105aac08ae38f4e367a2e5939d5cee4
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-03-30 21:48:14 +0530

    routes-added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Login } from './components/login/login';
+import { Projects } from './components/projects/projects';
+import { Register } from './components/register/register';
+import { Students } from './components/students/students';
+import { SubmitProject } from './components/submit-project/submit-project';
 
 export const routes: Routes = [
     {
@@ -12,7 +17,24 @@ export const routes: Routes = [
         component:Dashboard
     },
     {
-        
+        path:'login',
+        component:Login
+    },
+    {
+        path:'projects',
+        component:Projects
+    },
+    {
+        path:'register',
+        component:Register
+    },
+    {
+        path:'students',
+        component:Students
+    },
+    {
+        path:'submit-porject',
+        component:SubmitProject
     }
 
 ];
