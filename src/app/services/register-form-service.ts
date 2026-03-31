6bb9dc223d9f23d76b832fba84806f1026e17fff
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-03-31 22:11:48 +0530

    register-form-service-added

--- src/app/services/register-form-service.ts
+++ src/app/services/register-form-service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterFormService {
+
+  httpClient = inject(HttpClient)
   
 }
