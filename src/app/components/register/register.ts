84bd8e175e82b2fb3fa742a57906a0abb8d5c91e
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-03-31 22:03:05 +0530

    form-obj-added

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
