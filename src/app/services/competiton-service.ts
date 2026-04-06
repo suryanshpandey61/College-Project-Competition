9de4e9cb2e980c7284cf72234f1e95dcb4f59e3f
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-04-06 14:20:26 +0530

    cost-url-added

--- src/app/services/competiton-service.ts
+++ src/app/services/competiton-service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { constant } from '../constant/constant';
+import { CompetitonModel } from '../model/competition.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +9,13 @@ import { inject, Injectable } from '@angular/core';
 export class CompetitonService {
 
   private httpClient = inject(HttpClient);
+
+createCompetition(obj:CompetitonModel){
+    return this.httpClient.post(constant.API_URL+"competiton",obj)
+}
+
+fetchCompetitionDetails(){
+  
+}
   
 }
