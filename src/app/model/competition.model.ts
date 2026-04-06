4ced6cfe6742f0aa986f5bb5c30cf20c20efe91a
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-04-06 13:59:33 +0530

    model-added

--- src/app/model/competition.model.ts
+++ src/app/model/competition.model.ts
@@ -1,9 +1,9 @@
 export class CompetitonModel{
 
   competitionId: number,
-  title: "string",
-  "description": "string",
-  "startDate": "2026-04-06T07:57:33.691Z",
-  "endDate": "2026-04-06T07:57:33.691Z",
-  "status": "string"
+  title:string,
+  description:string,
+  startDate:string,
+  endDate:string,
+  status:string
 }
