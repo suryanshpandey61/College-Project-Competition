a67d84eecebe674ac672d211a367d75ec8867f78
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-04-06 10:08:42 +0530

    login-issue-resolved

---
+++ src/app/components/competition/competition.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition',
+  imports: [],
+  templateUrl: './competition.html',
+  styleUrl: './competition.css',
+})
+export class Competition {
+
+}
