ec2674e0a3892eff86c9f9a51b8fb6d35cf4ccd7
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-04-07 15:27:58 +0530

    modified-model

--- src/app/components/submit-project/submit-project.ts
+++ src/app/components/submit-project/submit-project.ts
@@ -4,10 +4,11 @@ import { ActivatedRoute } from '@angular/router';
 import { CompetitonModel } from '../../model/competition.model';
 import { DatePipe } from '@angular/common';
 import { ProjectSubmissionModel } from '../../model/project-submission.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-submit-project',
-  imports: [DatePipe],
+  imports: [DatePipe,FormsModule],
   templateUrl: './submit-project.html',
   styleUrl: './submit-project.css',
 })
