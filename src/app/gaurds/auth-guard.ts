65547cc716bb4d844619955d07590e7a6e079a2a
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-04-07 16:27:34 +0530

    auth-gaurd-updated

--- src/app/gaurds/auth-guard.ts
+++ src/app/gaurds/auth-guard.ts
@@ -4,5 +4,8 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const userId = sessionStorage.getItem('user-id');
+   if(userId) return true
+   else{
    
+   }
 };
