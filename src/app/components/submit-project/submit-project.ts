4e58e8d8d82346e596666df92ea008acf5547437
suryanshpandey61 <suryansh.knipss061@gmail.com>
2026-04-07 14:55:58 +0530

    routes-updated

--- src/app/components/submit-project/submit-project.ts
+++ src/app/components/submit-project/submit-project.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CompetitonService } from '../../services/competiton-service';
 
 @Component({
   selector: 'app-submit-project',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './submit-project.css',
 })
 export class SubmitProject {
+
+  competitionService = inject(CompetitonService)
 
 }
